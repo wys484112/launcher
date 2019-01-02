5cdab1f7169135b899311f842f84d20f5f77ab33
wys484112@163.com <wys484112@163.com>
2019-01-02 12:46:03 +0800

    update

--- app/src/main/java/com/example/launcher/MainAllAppsFragment.java
+++ app/src/main/java/com/example/launcher/MainAllAppsFragment.java
@@ -15,6 +15,7 @@
 package com.example.launcher;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
 import android.graphics.Color;
@@ -24,20 +25,27 @@ import android.os.Handler;
 import android.support.v17.leanback.app.BackgroundManager;
 import android.support.v17.leanback.app.BrowseFragment;
 import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.BaseGridView;
+import android.support.v17.leanback.widget.FocusHighlightHelper;
 import android.support.v17.leanback.widget.HeaderItem;
 import android.support.v17.leanback.widget.ImageCardView;
+import android.support.v17.leanback.widget.ItemBridgeAdapterShadowOverlayWrapper;
 import android.support.v17.leanback.widget.ListRow;
 import android.support.v17.leanback.widget.ListRowPresenter;
+import android.support.v17.leanback.widget.OnChildSelectedListener;
 import android.support.v17.leanback.widget.OnItemViewClickedListener;
 import android.support.v17.leanback.widget.OnItemViewSelectedListener;
 import android.support.v17.leanback.widget.Presenter;
 import android.support.v17.leanback.widget.Row;
 import android.support.v17.leanback.widget.RowPresenter;
+import android.support.v17.leanback.widget.ShadowOverlayHelper;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -73,7 +81,7 @@ public class MainAllAppsFragment extends BrowseFragment implements LoaderManager
     private BackgroundManager mBackgroundManager;
     private ArrayObjectAdapter mRowsAdapter;
 
-    private static final int NUM_COLS = 6;//一行显示5个应用
+    private static final int NUM_COLS = 5;//一行显示5个应用
 
     static List<String> favorites = Arrays.asList("com.example.launcher/com.example.launcher.MainActivity",
             "com.android.tv.settings/com.android.tv.settings.MainSettings",
@@ -87,44 +95,66 @@ public class MainAllAppsFragment extends BrowseFragment implements LoaderManager
     @Override
     public void onLoadFinished(Loader<List<AppInfo>> loader, List<AppInfo> appInfos) {
         appInfos = extractFavorites(appInfos);
+        int size=appInfos.size();
+        final int rows=size/NUM_COLS+1;
 
-        mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
+        mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter(){
+            @Override
+            protected void initializeRowViewHolder(RowPresenter.ViewHolder holder) {
+                super.initializeRowViewHolder(holder);
+
+                final ViewHolder rowViewHolder = (ViewHolder) holder;
+                Context context = holder.view.getContext();
+
+                // set wrapper if needed
+
+
+                rowViewHolder.getGridView().setNumRows(rows);
+            }
+        });
         AppCardPresenter cardPresenter = new AppCardPresenter();
 
-        int size=appInfos.size();
-        int rows=size/NUM_COLS+1;
+
         Log.d("wwww", "rows=="+rows);
         Log.d("wwww", "size=="+size);
 
-        int i;
-        for (i = 0; i < rows; i++) {
-
-            ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(cardPresenter);
-            int start=(i)*NUM_COLS;
-            Log.d("wwww", "i==" + i);
-
-            if((i+1)==rows){
-                Log.d("wwww", "add111==");
-
-                for (int j = start; j < size; j++) {
-                    Log.d("wwww", "add111==");
-
-                    listRowAdapter.add(appInfos.get(j));
-                }
-            }else{
-                Log.d("wwww", "add22==");
-
-                for (int j = 0; j < NUM_COLS; j++) {
-                    Log.d("wwww", "add22==");
-
-                    listRowAdapter.add(appInfos.get(start+j));
-                }
-//                HeaderItem header = new HeaderItem(i, MovieList.MOVIE_CATEGORY[i]);
-            }
-
+        ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(cardPresenter);
+        for(int i=0;i<size;i++){
+            listRowAdapter.add(appInfos.get(i));
+        }
             mRowsAdapter.add(new ListRow(null, listRowAdapter));
 
-        }
+
+//
+//        int i;
+//        for (i = 0; i < rows; i++) {
+//
+//            ArrayObjectAdapter listRowAdapter = new ArrayObjectAdapter(cardPresenter);
+//            int start=(i)*NUM_COLS;
+//            Log.d("wwww", "i==" + i);
+//
+//            if((i+1)==rows){
+//                Log.d("wwww", "add111==");
+//
+//                for (int j = start; j < size; j++) {
+//                    Log.d("wwww", "add111==");
+//
+//                    listRowAdapter.add(appInfos.get(j));
+//                }
+//            }else{
+//                Log.d("wwww", "add22==");
+//
+//                for (int j = 0; j < NUM_COLS; j++) {
+//                    Log.d("wwww", "add22==");
+//
+//                    listRowAdapter.add(appInfos.get(start+j));
+//                }
+//            }
+//                HeaderItem header = new HeaderItem(0, "afsdf");
+//
+//            mRowsAdapter.add(new ListRow(null, listRowAdapter));
+//
+//        }
 
 
 
@@ -139,17 +169,28 @@ public class MainAllAppsFragment extends BrowseFragment implements LoaderManager
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        Log.i(TAG, "onCreate");
-        super.onActivityCreated(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         prepareBackgroundManager();
 
         setupUIElements();
 
-//        loadRows();
-
         setupEventListeners();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onActivityCreated(savedInstanceState);
+
+
         LauncherAppState.setApplicationContext(getActivity());
 
         if (savedInstanceState == null) {
@@ -159,6 +200,20 @@ public class MainAllAppsFragment extends BrowseFragment implements LoaderManager
         }
 
     }
+
+
+//    /*
+//    * 去掉Title显示
+//    * */
+//    @Override
+//    public void installTitleView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+//        super.installTitleView(inflater, parent, savedInstanceState);
+//    }
+//
+//    @Override
+//    public View onInflateTitleView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+//        return null;
+//    }
 
     @Override
     public void onDestroy() {
@@ -198,9 +253,9 @@ public class MainAllAppsFragment extends BrowseFragment implements LoaderManager
     private void setupUIElements() {
         // setBadgeDrawable(getActivity().getResources().getDrawable(
         // R.drawable.videos_by_google_banner));
-        setTitle(getString(R.string.browse_title)); // Badge, when set, takes precedent
-        showTitle(true);
-//        setTitleView(null);
+//        setTitle(getString(R.string.browse_title)); // Badge, when set, takes precedent
+        showTitle(false);
+        setTitleView(null);
 //        setTitle(getString(R.string.browse_title)); // Badge, when set, takes precedent
         // over title
         setHeadersState(HEADERS_DISABLED);
